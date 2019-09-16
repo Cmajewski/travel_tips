9ecc28586f545b4c3354f3d73f26088f42f90918
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-16 04:22:47 +0000

    if statements

--- lib/travel_tips/cli.rb
+++ lib/travel_tips/cli.rb
@@ -1,49 +1,50 @@
 
-class TravelTips::CLI
+module TravelTips
+  class CLI
   attr_reader :spots
-  def run
-      welcome
-      menu
-      goodbye
 
-  end
+    def run
+        welcome
+        menu
+        goodbye
 
-  def welcome
-    puts "Looking for an affordable spot to travel this Fall (Y/N)?"
-    answer=gets.chomp
-    if answer=="N"
-      puts "Staycations are the best vacations!"
-      exit
-    elsif answer=="Y"
-      spots
-    else
-      puts "Im not sure what you mean by that. Try selecting (Y/N)."
     end
-  end
 
-  def spots
-    @spots=Spots.all_with_index
-  end
-
-  def menu
-    input=nil
-    while input != "exit"
-    puts "Enter the number of the destination you would like to learn more about or type list for the full list:"
-    input=gets.chomp
-    case input
-    when "1"
-      puts "lobsters"
-    when "2"
-      puts "beaches"
-    when "3"
-      puts "ranch"
-    when "list"
-      spots
+    def welcome
+      puts "Looking for an affordable spot to travel this Fall (Y/N)?"
+      answer=gets.chomp
+      if answer=="N"
+        puts "Staycations are the best vacations!"
+        exit
+      elsif answer=="Y"
+        spots
+      else
+        puts "Im not sure what you mean by that. Try selecting (Y/N)."
+      end
     end
-  end
-  end
 
-  def goodbye
-    puts "Go book your Fall trip!!"
+    def spots
+      @spots=TravelTips::Spots.all_with_index
+    end
+
+    def menu
+      input=nil
+      while input != "exit"
+      puts "Enter the number of the destination you would like to learn more about or type list for the full list:"
+      input=gets.chomp
+        if input!="list" && input.to_i>0 && input>to_i<=20
+          puts @spots[input.to_i-1]
+        elsif input="list"
+          @spots
+        else
+          puts "Not sure what you mean, please pick a number or list."
+        end
+      end
+    end
+
+    def goodbye
+      puts "Go book your Fall trip!!"
+    end
+
   end
 end
--- lib/travel_tips/scraper.rb
+++ lib/travel_tips/scraper.rb
@@ -1,2 +1,5 @@
-class Scraper
+puts "scraper"
+module TravelTips
+  class Scraper
+  end
 end
