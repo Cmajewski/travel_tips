80687e46a16c47c6ace85589bc47d88cb121efa6
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-16 16:58:26 +0000

    scraping initial

--- lib/travel_tips/scraper.rb
+++ lib/travel_tips/scraper.rb
@@ -1,4 +1,15 @@
 module TravelTips
   class Scraper
+
+    def get_file
+
+      @url=Nokogiri::HTML(open("https://www.travelandleisure.com/trip-ideas/fall-vacations/affordable-destinations-for-fall-travel"))
+      x=@url.css("article-content-container").collect {|x| x}
+      binding.pry
+    end
+
+    def scrape_names
+
+    end
   end
 end
