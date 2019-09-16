4fba09f03fe086b1f0b1d5f4bd1f1f98cdaddddb
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-16 11:45:13 +0000

    object establishment

--- lib/travel_tips/spots.rb
+++ lib/travel_tips/spots.rb
@@ -1,14 +1,28 @@
-puts "spots"
 module TravelTips
   class Spots
+    attr_accessor :name, :description, :plane_ticket, :hotel_rate
+    @@all=[]
 
-    def self.all_with_index
-      puts "
-      1. maine
-      2. RI
-      3. texas
-      4. cali"
-      #return all the spots in a numbered order
+    def initialize
     end
+
+    def self.all
+      puts "[maine, RI, texas, cali]"
+
+      trip_1=Spots.new
+      trip_1.name="vk1"
+      trip_1.description="its so cool"
+      trip_1.plane_ticket="$250"
+      trip_1.hotel_rate="$100"
+
+      trip_2=Spots.new
+      trip_2.name="vk2"
+      trip_2.description="not that cool"
+      trip_2.plane_ticket="$10"
+      trip_2.hotel_rate="$10"
+      #return all the spots in a numbered order
+      [trip_1,trip_2]
+    end
+
   end
 end
