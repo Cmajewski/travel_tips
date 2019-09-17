987451129a405ab8a6a626883ad694eff074285a
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-17 03:00:06 +0000

    scrape edits

--- lib/travel_tips/scraper.rb
+++ lib/travel_tips/scraper.rb
@@ -1,31 +1,29 @@
 module TravelTips
   class Scraper
 
-      def get_file
-        Nokogiri::HTML(open("https://www.travelandleisure.com/trip-ideas/fall-vacations/affordable-destinations-for-fall-travel"))
-      end
+    def get_file
+      Nokogiri::HTML(open("https://www.travelandleisure.com/trip-ideas/fall-vacations/affordable-destinations-for-fall-travel"))
+    end
 
-      def scrape_name
-        names=[]
-      # flights=[]
-      # hotel=[]
-          self.get_file.css("div.article-content-container h2").each  do |content|
-          reformat=content.text.split(".")
-          names<<reformat[1]
-        end
+    def scrape_name
+      names=[]
+      self.get_file.css("div.article-content-container h2").each  do |content|
+        reformat=content.text.split(".")
+        names<<reformat[1]
       end
+    end
 
     def scrape_description
-        description=[]
-        self.get_file.css("p").each do |words|
-          if /^Average/ =~ words
-          else
-            description<<words.text
-          end
+      all_descriptions=[]
+      self.get_file.css("p").each do |words|
+        if /^Average/ =~ words
+        else
+          all_descriptions<<words.text
         end
-        new=description.drop(3)
-
-
+      end
+      description=all_descriptions.drop(3)
+    end
+        binding.pry
 
         #do |paragraph|
         #   description<<paragraph
@@ -42,7 +40,7 @@ module TravelTips
         #flights<<flight.text
           #names<<content.css("h2").text
           #@x=content.css("p")
-          end
+
 
 
 
