86677d890d513e7800c3b5ea79adf5a75ca476cc
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-17 02:50:21 +0000

    scraping description fixed

--- lib/travel_tips/scraper.rb
+++ lib/travel_tips/scraper.rb
@@ -1,27 +1,39 @@
 module TravelTips
   class Scraper
 
-    def get_file
-      Nokogiri::HTML(open("https://www.travelandleisure.com/trip-ideas/fall-vacations/affordable-destinations-for-fall-travel"))
-    end
+      def get_file
+        Nokogiri::HTML(open("https://www.travelandleisure.com/trip-ideas/fall-vacations/affordable-destinations-for-fall-travel"))
+      end
 
-    def scrape_name
+      def scrape_name
         names=[]
       # flights=[]
       # hotel=[]
           self.get_file.css("div.article-content-container h2").each  do |content|
           reformat=content.text.split(".")
           names<<reformat[1]
-
         end
-        binding.pry
+      end
 
-
-    end
-
-    def self.scrape_description
+    def scrape_description
         description=[]
-        description<<self.get_file.css("div.p").text
+        self.get_file.css("p").each do |words|
+          if /^Average/ =~ words
+          else
+            description<<words.text
+          end
+        end
+        new=description.drop(3)
+
+
+
+        #do |paragraph|
+        #   description<<paragraph
+        #
+        # end
+        #
+
+
         #  flights<<content.css('p:contains("Average round-trip ticket:")').text.strip
         #  hotel<<content.css('p:contains("Average nightly hotel rate:")').text.strip
 
